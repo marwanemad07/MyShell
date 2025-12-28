58680be77d4ee0cc25911998fe9bdb1edbb68a44
Marwan Emad <marwanemad910@gmail.com>
2025-12-29 01:49:12 +0200

    forgot to add the file into the stage :)

---
+++ src/Commands/HistoryCommand.cs
@@ -0,0 +1,12 @@
+namespace MyShell.Core.Commands
+{
+    public class HistoryCommand : ICommand
+    {
+        public string Name => "history";
+
+        public int Execute(List<string> args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
